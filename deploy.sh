82b6f3d199a661af0cc69dabc4f6524140fb6a59
deployer <deployer-user@deployer-server>
2021-10-06 18:06:17 -0400

    adding scipt deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#deployer script v1.0
+if [ $# == 0 ]
+then
+        echo "Please set arguments prod or dev" && exit 1
+fi
+
+function git_commit () {
+
+git commit -m "deploy.sh script commit"
+}
+
+
+for args in "$@"
+do
+        case $args in
+                prod) git checkout master && npm run build  deployer@papik-prod:/var/www/papik.conf && git_commit;;
+                dev) git checkout development && npm run build-dev  deployer@papik-prod:/var/www/dev.papik.conf && git_commit;;
+        esac
+done
